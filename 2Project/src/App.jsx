677ee9e0a53a8b822f8e7326d80d5afefe777eb8
rubulgithub <114800261+rubulgithub@users.noreply.github.com>
2023-11-27 22:07:21 +0530

    filter fun

--- 2Project/src/App.jsx
+++ 2Project/src/App.jsx
@@ -3,12 +3,15 @@ import React, { useState } from "react";
 const TodoList = () => {
   const [todos, setTodos] = useState([]);
   const [newTodo, setNewTodo] = useState("");
+  const [filter, setFilter] = useState("all");
 
+  //Adding a todo
   const addTodo = () => {
     setTodos([{ id: Date.now(), text: newTodo, completed: false }, ...todos]);
     setNewTodo("");
   };
 
+  //Toggle Complete
   const completeTodo = (id) => {
     setTodos((prev) =>
       prev.map((prevTodo) =>
@@ -19,22 +22,42 @@ const TodoList = () => {
     );
   };
 
+  //Toggle all
   const toggleAllTodos = () => {
     setTodos((prev) =>
       prev.map((todo) => ({ ...todo, completed: !todo.completed }))
     );
   };
 
+  //Delete todo
   const removeTodo = (id) => {
     setTodos((pre) => pre.filter((todo) => todo.id !== id));
   };
 
+  //filter the todos
+  const filterTodos = (filter) => {
+    setFilter(filter);
+  };
+
+  //filtered items
+  const filteredTodos = todos.filter((todo) => {
+    if (filter === "all") {
+      return true;
+    } else if (filter === "incomplete") {
+      return todo.completed;
+    } else if (filter === "complete") {
+      return !todo.completed;
+    }
+  });
   return (
     <div>
       <h1>Todo List</h1>
+      <button onClick={() => filterTodos("all")}>Show all</button>
+      <button onClick={() => filterTodos("complete")}>Show complete</button>
+      <button onClick={() => filterTodos("incomplete")}>Show incomplete</button>
       <button onClick={toggleAllTodos}>Toggle all</button>
       <ul>
-        {todos.map((todo, id) => (
+        {filteredTodos.map((todo, id) => (
           <li
             key={todo.id}
             style={{ textDecoration: todo.completed ? "line-through" : "none" }}
